8cd480e50e1f746d414f5d5fccbb8881f435b319
trangthupham02051998 <trangthupham02051998@gmail.com>
2021-08-05 23:17:44 +0700

    First Commit

---
+++ src/staffJSON.js
@@ -0,0 +1,44 @@
+export const StaffData = [
+    {
+      "FullName":"Nguyễn Văn A",
+      "BornDays":"01/01/2002",
+      "Deparments":"Sales",
+      "LeaveDaysLeft":"3",
+      "WorkDaysMore":"5"
+    },
+    {
+      "FullName":"Nguyễn Văn B",
+      "BornDays":"01/01/2001",
+      "Deparments":"IT",
+      "LeaveDaysLeft":"4",
+      "WorkDaysMore":"5"
+    },
+    {
+      "FullName":"Nguyễn Văn C",
+      "BornDays":"01/01/2000",
+      "Deparments":"HR",
+      "LeaveDaysLeft":"6",
+      "WorkDaysMore":"2"
+    },
+    {
+      "FullName":"Nguyễn Văn D",
+      "BornDays":"01/01/2000",
+      "Deparments":"IT",
+      "LeaveDaysLeft":"6",
+      "WorkDaysMore":"2"
+    },
+    {
+      "FullName":"Nguyễn Văn E",
+      "BornDays":"01/01/1999",
+      "Deparments":"Sales",
+      "LeaveDaysLeft":"3",
+      "WorkDaysMore":"3"
+    },
+    {
+      "FullName":"Nguyễn Văn F",
+      "BornDays":"01/01/1998",
+      "Deparments":"IT",
+      "LeaveDaysLeft":"8",
+      "WorkDaysMore":"3"
+    }
+  ];
