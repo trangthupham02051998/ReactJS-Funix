b9cd38b0d188af5cb4adc0ee9ba536490be35577
trangthupham02051998 <you@example.com>
2021-08-15 09:42:37 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import { DISHES } from './shared/dishes';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import {Component} from "react";
@@ -6,16 +6,27 @@ import React from 'react'
 import Menu from './components/MenuComponent';
 
 class App extends Component {
+    // vì n là constructor của class
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+    // hàm render dùng để hiển thị nội dung
     render() {
         return (
+
             <div className="App">
+
                 <Navbar dark color="primary">
                     <div className="container">
                         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
                     </div>
                 </Navbar>
-                <Menu />
+                <Menu dishes={this.state.dishes} />
             </div>
+
 
         );
     }
