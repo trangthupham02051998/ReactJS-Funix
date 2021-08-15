cf0bdf0dd5821d9fe4498bef40ac813e342ab324
trangthupham02051998 <you@example.com>
2021-08-15 10:02:46 +0700

    Functional Components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import {Component} from "react";
 import React from 'react'
 import Menu from './components/MenuComponent';
+import Main from './components/MainComponent';
 
 class App extends Component {
     // vì n là constructor của class
@@ -16,18 +17,15 @@ class App extends Component {
     // hàm render dùng để hiển thị nội dung
     render() {
         return (
-
             <div className="App">
-
                 <Navbar dark color="primary">
                     <div className="container">
                         <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
                     </div>
                 </Navbar>
                 <Menu dishes={this.state.dishes} />
+                <Main />
             </div>
-
-
         );
     }
 }
---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle } from 'reactstrap';
+
+
+function RenderDish({dish}) {
+
+
+
+}
+
+function RenderComments({comments}) {
+
+
+
+}
+
+const  DishDetail = (props) => {
+    //Return về div để không lỗi
+    return (
+        <div></div>
+    )
+}
+
+export default DishDetail;
